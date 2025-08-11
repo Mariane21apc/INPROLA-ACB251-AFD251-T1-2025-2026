e935d8e2bf14d1deba11dd7e88bce83faf63e70f
Mariane21apc <msolivar@student.apc.edu.ph>
2025-08-11 03:19:24 +0000

    added java file

---
+++ week04/LoopExercise.java
@@ -0,0 +1,24 @@
+import java.util..scanner;
+
+class LoopExercise
+public static void main(String[] args) {
+    Scanner = myObj = new scanner(System.in);
+
+    System.out.println("Enter two value:");
+
+    int n = 5;
+    int a = 4;
+    
+    // Numerical input
+    int Name = myObj.nextInt();
+    int Age = myObj.nextInt();
+
+     // Output input by user
+    System.out.println("Name: " + a);
+    System.out.println("Age: " + b);
+
+     // for loop  
+    for (int i = 1; i <= n; ++i) {
+      System.out.println("Java is fun"+" from: "+i+ " to: "+n);
+  }
+}
