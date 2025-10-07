eb400990201697ebe35667206a23d3dabf92cdf2
Mariane21apc <msolivar@student.apc.edu.ph>
2025-10-07 07:05:13 +0000

    Py Harrypotter

---
+++ Sometingnew.py/Harrypotter.py
@@ -0,0 +1,15 @@
+power = input()
+if power == "y":
+	print("Are you selfish(y/n) ")
+	selfish = input()
+	if selfish == "y":
+		print("Slytherin!")
+	else:
+		print("Gryffindor!")
+else:
+	print("Are you analytical (y/n) ")
+	analytical - input()
+	if analytical == "y":
+		print("Ravenclaw!")
+	else:
+		print("Hufflepuff!")
