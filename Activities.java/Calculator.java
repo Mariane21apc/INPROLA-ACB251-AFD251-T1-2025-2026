ef12767c7eebfd2cd6e74f71b2ec3ad229f3cb04
Mariane21apc <msolivar@student.apc.edu.ph>
2025-10-13 02:15:38 +0000

    java Calculator

---
+++ Activities.java/Calculator.java
@@ -0,0 +1,33 @@
+public class Calculator implements SampleCalculator {
+
+     private int sum=0;
+     private int difference=0; 
+     private int product=0; 
+     private int quotient=0;
+
+     public void add(int i) {
+        sum+=i;
+    } 
+     
+     public void subtract(int i) {
+         difference-=i; 
+    } 
+    
+    public void multiply(int i) { 
+        product*=i; 
+    
+    } public void divide(int i) { 
+        quotient/=i; 
+    
+    } public static void main(String args[]) { 
+        
+        Calculator calc = new Calculator(); 
+        calc.add(12); System.out.println("Add result:"+ calc.sum); 
+
+        calc.subtract(12); 
+        System.out.println("Subtract result:"+ calc.difference); 
+        
+        calc.multiply(12); 
+        System.out.println("Multiply result:"+ calc.product); 
+        calc.divide(12); System.out.println("Divide result:"+ calc.quotient); 
+        
---
+++ Activities.java/SampleCalculator.java
@@ -0,0 +1,21 @@
+// interfaces
+*/
+
+implemetns keyword in your class
+declaration
+
+<access modifier> class ClassName implements <interfaces, interfaces1..interfacesN> {
+
+
+}
+
+*/
+
+interface SampleCalculator {
+
+	public void add(int i);
+	public void subtract(int i);
+	public void multiply(int i);
+	public void divide(int i);
+	}
+}	
